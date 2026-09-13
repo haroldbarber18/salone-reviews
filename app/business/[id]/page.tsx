4fb27a7d382317f6596bad9afff6833789123e5b
haroldbarber18 <info@salonereviews.com>
2026-09-13 23:12:43 +0100

    Fix shop page build extra bracket

--- app/business/[id]/page.tsx
+++ app/business/[id]/page.tsx
@@ -687,7 +687,6 @@ export default function BusinessPage() {
                 </div>
               </div>
             )}
-            )}
           </div>
 
           {user && photos.length > 0 && (
